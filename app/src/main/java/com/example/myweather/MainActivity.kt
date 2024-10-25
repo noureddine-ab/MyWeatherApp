85f2d051aeeefd4678a35fc3d9597b8745453ae2
noureddine <bounarda1234@gmail.com>
2024-10-25 17:33:08 +0100

    nothing

--- app/src/main/java/com/example/myweather/MainActivity.kt
+++ app/src/main/java/com/example/myweather/MainActivity.kt
@@ -22,25 +22,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    WeatherPage()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyWeatherTheme {
-        Greeting("Android")
     }
 }
